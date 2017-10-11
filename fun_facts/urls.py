3c0d897f77969a2e039224c03f8615c7ddc49365
@eduardo_78d <eduardo78d@gmail.com>
2017-10-10 20:04:57 -0500

    crud

--- fun_facts/urls.py
+++ fun_facts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from .views import FunFactListView, FunFactCreate, FunFactDetailView
+from .views import FunFactListView, FunFactCreate, FunFactDetailView, FunFactEditView, FunFactDeleteView
+
+#r'^(?P<slug>[-\w]+)/$
 
 urlpatterns = [
   url(r'^$', FunFactListView.as_view(), name='fun_fact_list'),
   url(r'^nuevo$', FunFactCreate.as_view(), name='fun_fact_new'),
   url(r'^(?P<pk>[0-9]+)/$',FunFactDetailView.as_view(), name='fun_fact_detail'),
-  url(r'^(?P<pk>[0-9]+)/eliminar$',FunFactDetailView.as_view(), name='fun_fact_delete'),
-  url(r'^(?P<pk>[0-9]+)/actualizar$',FunFactDetailView.as_view(), name='fun_fact_update'),
+  url(r'^(?P<pk>[0-9]+)/editar$',FunFactEditView.as_view(), name='fun_fact_edit'),
+  url(r'^(?P<pk>[0-9]+)/eliminar$',FunFactDeleteView.as_view(), name='fun_fact_delete'),
 ]
--- fun_facts/views.py
+++ fun_facts/views.py
@@ -1,3 +1,7 @@
+from django.core.urlresolvers import reverse
+from django.urls import reverse_lazy
+
+
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
@@ -5,10 +9,10 @@ from django.core.paginator import PageNotAnInteger
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 from django.contrib.messages.views import SuccessMessageMixin
-
-from django,views.generic import DeleteView
 
 from .models import FunFact
 from .forms import FunFactForm
@@ -35,23 +39,30 @@ class FunFactCreate(SuccessMessageMixin, CreateView):
   model = FunFact
   form_class = FunFactForm
   success_url = '/datos/nuevo'
-  success_message = 'Successfully Added a Post entry'
+  success_message = 'Dato creado exitosamente'
 
   def form_valid(self, form):
     self.object = form.save(commit=False)
-    #self.object.author = self.request.user
     return super(FunFactCreate, self).form_valid(form)
 
 class FunFactDetailView(DetailView):
-  model = FunFact
+  model = FunFact   
 
   def get_context_data(self, **kwargs):
     context = super(FunFactDetailView, self).get_context_data(**kwargs)
     return context
 
+class FunFactEditView(SuccessMessageMixin, UpdateView):
+  model = FunFact
+  form_class = FunFactForm
+  success_message = 'Dato editado exitosamente'
+  template_name_suffix = '_update_form'
 
-class FunFactDeleteView(DeleteView):
+  def get_success_url(self):
+    return reverse("fun_fact_edit", kwargs={'pk': self.object.pk})
 
-  def get_object(self, queryset=None):
-    obj =  syper(FunFactDeleteView, self).get_object
-    
+class FunFactDeleteView(SuccessMessageMixin, DeleteView):
+  model = FunFact
+  success_url = reverse_lazy('fun_fact_list')
+  success_message = 'Dato eliminado correctamente'
+
